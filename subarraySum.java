c38e59886b59ba280738e0cb9991bcb162a2b2e6
utkarsh08122 <utkarshsingh08122002@gmail.com>
2026-08-18 23:39:25 +0530

    560

---
+++ subarraySum.java
@@ -0,0 +1,28 @@
+
+// question 560
+// worst code 
+
+// public class subarraySum {
+//     public int subarraySum(int[] nums, int k) {
+//         int j =0;
+//         int count = 0;
+
+//         for(int i =0 ; i<nums.length ; i++){
+//           int sum =0;
+//           j=i;
+//           while( j<nums.length){
+//           sum = sum + nums[j];
+//           if(sum == k){
+
+//           count ++;
+//           }
+//           j++;
+//           }
+
+//         }
+//         return count ;
+        
+//     }
+// } {
+    
+// }
