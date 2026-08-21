570f1bf18fec0e40984bdbaa04f6308ae5ee6cb8
utkarsh08122 <utkarshsingh08122002@gmail.com>
2026-08-22 00:41:48 +0530

    1248

---
+++ intersection1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class intersection {
+    public List<Integer> intersection(int[][] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+       
+        for (int i = 0; i < nums.length; i++) {
+
+           
+            for (int j = 0; j < nums[i].length; j++) {
+
+                int num = nums[i][j];
+
+               
+                if (!map.containsKey(num)) {
+                    map.put(num, 1);
+                } else {
+                    map.put(num, map.get(num) + 1);
+                }
+            }
+        }
+
+       
+        for (int num : map.keySet()) {
+            if (map.get(num) == nums.length) {
+                result.add(num);
+            }
+        }
+
+        Collections.sort(result);
+
+        return result;
+    }
+}
